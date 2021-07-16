4d0ebbff39ae513ff829f71a26d808314245d6ed
Sarvadnya mahajan <80883695+SarvadnyaMahajan@users.noreply.github.com>
2021-07-16 20:59:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+block_image_width = 30;
+block_image_height = 30;
+
+player_x = 10;
+player_y = 10;
+
+var player_object = "";
+var player_object2 = "";
+
+function player_update()  
+{
+
+    fabric.image.fromURLL("player.png"), function (Img) {
+
+        player_object = Img;
+
+        player_object.scaletoWidth(150);
+        player_object.scaletoHeight(140);
+        player_object.set({
+        top:player_y,
+        top:player_x
+        });
+        canvas.add(player_object);
+
+    }    
+}
+
+function new_image() 
+{
+
+    fabric.image.fromURL(get_image), function(Img) {
+    block_image_object = Img;
+    
+    block_image_object.scaletoWidth(block_image_width);
+    block_image_object.scaletoHeight(block_image_height);
+    block_image_object.set({
+
+        top:player_y,
+        left:player_x
+        });
+        canvas.add(block_image_object)
+    };
+
+}
